35c7efe6cf2ec8c7d9b1b0a77ef94acaebf5e58e
joodk1 <joodkhalled@hotmail.com>
2024-03-20 20:31:48 +0300

    fixed articles

--- flask_shayek/flaskblog/routes.py
+++ flask_shayek/flaskblog/routes.py
@@ -1,21 +1,20 @@
 from flask import render_template, url_for, flash, redirect
 from flaskblog import app
 from flaskblog.forms import RegistrationForm, LoginForm, RegistrationRequestForm
-from flaskblog.models import User, Post
 
 posts = [
     {
-        'author': 'شيّـــك',
-        'title': 'النشرة الأولى',
-        'content': 'تجربة',
-        'date_posted': 'Mar 16, 2024'
+        'author': 'صحيفة مكة',
+        'title': 'الأميرة ساره بنت مساعد تكرم الفائزات بجائزة سيدة الباحة',
+        'content': 'رعت صاحبة السمو الملكي الأميرة سارة بنت مساعد بن عبدالعزيز حرم سمو أمير منطقة الباحة، بمركز الحسام للمعارض والمؤتمرات، حفل تكريم الفائزات بجائزة “سيدة الباحة” في نسختها الأولى والتي تعد إحدى مبادرات فرع وزارة الموارد البشرية والتنمية الاجتماعية بمنطقة الباحة لتمكين المرأة والإحتفاء بالسيدات المتميزات.',
+        'date_posted': 'Mar 20, 2024'
 
     },
     {
         'author': 'جريدة الرياض',
-        'title': 'أخبار اليوم',
-        'content': 'مرحبًا شيّـــك',
-        'date_posted': 'Mar 17, 2024'
+        'title': 'مجلس الوزراء: 27 من مارس يوماً رسمياً لـ السعودية الخضراء',
+        'content': 'رأس خادم الحرمين الشريفين الملك سلمان بن عبدالعزيز آل سعود ـ حفظه الله ـ، الجلسة التي عقدها مجلس الوزراء، الثلاثاء، في جدة.',
+        'date_posted': 'Mar 20, 2024'
 
     }
 ]
