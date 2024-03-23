c302e6e7e2260feac62c9dfc67bfecb64616bd0f
joodk1 <joodkhalled@hotmail.com>
2024-03-24 01:28:36 +0300

    added logout and modified nav bar
    added sessions and session roles to accommodate two types of users and fixed nav bar accordingly

--- flask_shayek/flaskblog/routes.py
+++ flask_shayek/flaskblog/routes.py
@@ -1,21 +1,18 @@
 import os
 from uuid import uuid4
 from flask import current_app as app
-from flask import render_template, url_for, flash, redirect, request, Flask, session ,jsonify
-from flaskblog import app, firebase
+from flask import render_template, url_for, flash, redirect, request, Flask, session, jsonify, abort
+from flaskblog import app, firebase, login_manager
 from flaskblog.forms import RegistrationForm, LoginForm, RegistrationRequestForm
-from werkzeug.utils import secure_filename
-import firebase_admin
-from firebase_admin import credentials, db, firestore, storage
-from flask import abort
 from flask_login import login_user, current_user, logout_user, login_required, UserMixin
-from flaskblog import app, login_manager
+import firebase_admin
+from firebase_admin import credentials, db, firestore, storage, auth
+from werkzeug.utils import secure_filename
 import random
 import string
-from firebase_admin import credentials, auth
 
 # Firebase Admin SDK Initialization
-cred = credentials.Certificate('/Users/noraaziz/Downloads/shayek-560ec-firebase-adminsdk-b0vzc-d1533cb95f.json')
+cred = credentials.Certificate('C:/Users/huaweii/Downloads/shayek-560ec-firebase-adminsdk-b0vzc-d1533cb95f.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://shayek-560ec-default-rtdb.firebaseio.com/',
     'storageBucket': 'shayek-560ec.appspot.com'
@@ -74,30 +71,28 @@ def login():
         }
         try:
             response = requests.post(f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}", json=request_payload)
-            response.raise_for_status()  # This will raise an exception for HTTP error responses
+            response.raise_for_status() 
             user_data = response.json()
-            # Here, you could set user data in Flask's session or whatever your app requires
+            session['logged_in'] = True
+            session['role'] = 'user'
             flash('تم تسجيل دخولك بنجاح', 'success')
             return redirect(url_for('home'))
         except requests.exceptions.HTTPError as e:
             error_json = e.response.json()
             error_message = error_json.get('error', {}).get('message', 'UNKNOWN_ERROR')
             flash(f'فشل تسجيل دخولك، راجع بريدك الإلكتروني وكلمة المرور. Error: {error_message}', 'danger')
+            pass
     return render_template('login.html', title='تسجيل الدخول', form=form)
 
 def upload_file_to_firebase_storage(file):
     if file:
-        # Ensure the filename is secure
         filename = secure_filename(file.filename)
-        # Create a reference to the upload path
         bucket = storage.bucket()
         blob = bucket.blob(f"company_docs/{filename}")
-        # Upload the file
         blob.upload_from_string(file.read(), content_type=file.content_type)
-        # Make the blob publicly viewable
         blob.make_public()
-        # Return the gs:// URL
         return f"gs://shayek-560ec.appspot.com/company_docs/{filename}"
+
 
 @app.route('/register_request', methods=['GET', 'POST'])
 def register_request():
@@ -119,7 +114,7 @@ def register_request():
 
         db.reference('registration_requests').push(registration_data)
 
-        flash('Your request has been submitted successfully!', 'success')
+        flash('تم رفع طلبكم بنجاح', 'success')
         return redirect(url_for('home'))
     else:
         return render_template('register_request.html', title='طلب تسجيل حساب', form=form)
@@ -151,10 +146,12 @@ def admin_login():
         if username == 'admin' and password == 'adminpass':
             user = User('admin')  
             login_user(user)
-            flash('Logged in successfully.')
+            session['logged_in'] = True
+            session['role'] = 'admin'
+            flash('تم تسجيل دخولك بنجاح', 'success')
             return redirect(url_for('admin_dashboard'))
         else:
-            flash('Login Unsuccessful. Please check username and password')
+            flash('فشل تسجيل دخولك، راجع بريدك الإلكتروني وكلمة المرور.')
     return render_template('admin_login.html')
 
 
@@ -190,11 +187,10 @@ def verify_request(request_id):
             return redirect(url_for('admin_dashboard'))
        
         try:
-            # Admin accepts the request - create user in Firebase Authentication
             user_password = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
             new_user = auth.create_user(
                 email=request_data['email'],
-                password=user_password,  # Ensure this password is securely generated or collected
+                password=user_password,
             )
             user_data ={
             'username': request_data['username'],
@@ -204,17 +200,14 @@ def verify_request(request_id):
             
             ref_user = db.reference('users').push(user_data)
             
-            # Update the request status in your database
             ref_request.update({'status': 'accepted', 'uid': new_user.uid})
             
-            flash('Registration request accepted and user created.', 'success')
+            flash('تم قبول طلب التسجيل وإنشاء الحساب', 'success')
             js_script = f"alert('{user_password} هو: {request_data['email']} الرقم السري للحساب ');"
             return f"<script>{js_script}</script><script>window.location.href = '{url_for('admin_dashboard')}';</script>"
         
         except Exception as e:
-            # Handle potential errors, such as email already exists
             flash(f'Error creating user: {str(e)}', 'danger')
-            # It's important to return here to prevent further execution which might rely on new_user being successfully created
             return redirect(url_for('admin_dashboard'))
 
         return redirect(url_for('admin_dashboard'))
@@ -223,8 +216,17 @@ def verify_request(request_id):
         return redirect(url_for('admin_dashboard'))
 
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.pop('role', None)
+    flash('تم تسجيل خروجك بنجاح', 'success')
+    return redirect(url_for('home'))
+
 
 @app.route('/admin/logout')
 def admin_logout():
     session.pop('admin_logged_in', None)
+    session.pop('role', None)
+    flash('تم تسجيل خروجك بنجاح', 'success')
     return redirect(url_for('admin_login'))
