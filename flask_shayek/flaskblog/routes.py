76ea327ecf1680df94bdbc192548a8056b7056be
Nouff0 <nouf0fahad145@gmail.com>
2024-03-27 23:11:09 +0300

    adminlogin

--- flask_shayek/flaskblog/routes.py
+++ flask_shayek/flaskblog/routes.py
@@ -12,7 +12,7 @@ import random
 import string
 
 # Firebase Admin SDK Initialization
-cred = credentials.Certificate('/Users/noraaziz/Downloads/shayek-560ec-firebase-adminsdk-b0vzc-d1533cb95f.json')
+cred = credentials.Certificate('/Users/lamiafa/Downloads/shayek-560ec-firebase-adminsdk-b0vzc-d1533cb95f.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://shayek-560ec-default-rtdb.firebaseio.com/',
     'storageBucket': 'shayek-560ec.appspot.com'
@@ -102,7 +102,37 @@ def login():
                 session['user_info'] = {'email': email, 'username': username}
                 flash('<i class="fas fa-check-circle me-3"></i> تم تسجيل دخولك بنجاح', 'success')
                 return redirect(url_for('user_home'))
-            elif user_role == 'admin':
+            else:
+                flash('<i class="fas fa-times-circle me-3"></i> فشل تسجيل دخولك، راجع بريدك الإلكتروني وكلمة المرور', 'danger')
+
+        except requests.exceptions.HTTPError as e:
+            error_json = e.response.json()
+            error_message = error_json.get('error', {}).get('message', 'UNKNOWN_ERROR')
+            flash(f'<i class="fas fa-times-circle me-3"></i> فشل تسجيل دخولك، راجع بريدك الإلكتروني وكلمة المرور', 'danger')
+    return render_template('login.html', title='تسجيل الدخول', form=form)
+
+
+
+
+@app.route('/adsecretlogin', methods=['GET', 'POST'])
+def adminlogin():
+    form = LoginForm()
+    if form.validate_on_submit():
+        api_key = "AIzaSyAXgzwyWNcfI-QSO_IbBVx9luHc9zOUzeY"
+        request_payload = {
+            "email": form.email.data,
+            "password": form.password.data,
+            "returnSecureToken": True
+        }
+        try:
+            response = requests.post(f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}", json=request_payload)
+            response.raise_for_status()
+            user_info = response.json()
+            email = form.email.data
+            user_role = determine_user_role(email)
+            session['logged_in'] = True
+            session['role'] = user_role 
+            if user_role == 'admin':
                 flash('<i class="fas fa-check-circle me-3"></i> تم تسجيل دخولك كمسؤول', 'success')
                 return redirect(url_for('admin_dashboard'))
             else:
@@ -112,7 +142,10 @@ def login():
             error_json = e.response.json()
             error_message = error_json.get('error', {}).get('message', 'UNKNOWN_ERROR')
             flash(f'<i class="fas fa-times-circle me-3"></i> فشل تسجيل دخولك، راجع بريدك الإلكتروني وكلمة المرور', 'danger')
-    return render_template('login.html', title='تسجيل الدخول', form=form)
+    return render_template('adsecretlogin.html', title='تسجيل الدخول', form=form)
+
+
+
 
 def fetch_username_from_database(email):
     user_ref = db.reference('users').order_by_child('email').equal_to(email).get()
